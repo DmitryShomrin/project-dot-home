90b880e03874b05b379570949f1ea7f197caee79
DmitryShomrin <dmitry.shomrin@yandex.ru>
2025-08-27 19:38:35 +0200

    Terraform code for backup-server & tailscale-node

---
+++ terraform/README.md
@@ -0,0 +1,9 @@
+## Proxmox terraform provider
+
+Docs: [Telmate/proxmox](https://registry.terraform.io/providers/Telmate/proxmox/latest/docs)
+
+Credentials:
+```bash
+export PM_USER="terraform-prov@pam"
+export PM_PASS="password"
+```
---
+++ terraform/backup-server/vm.tf
@@ -0,0 +1,70 @@
+resource "proxmox_vm_qemu" "vm" {
+    count = 1
+    
+    # VM General Settings
+    target_node = "proxmox-0"
+    vmid        = 101
+    name        = "backup-server"
+    description = "NAS/Backup server"
+
+    # VM Advanced General Settings
+    onboot = true
+
+    # VM OS Settings
+    clone_id   = "999"
+    full_clone = false
+
+    # VM System Settings
+    agent = 1
+    
+    # VM CPU Settings
+    cpu {
+        cores   = 4
+        sockets = 1
+        type    = "host"
+    }
+    
+    # VM Memory Settings
+    memory = 8192
+
+    scsihw = "virtio-scsi-pci"
+    # VM Network Settings
+    network {
+        id     = 0
+        bridge = "vmbr0"
+        model  = "virtio"
+    }
+
+    disk {
+        slot    = "ide0"
+        size    = "4M"
+        type    = "cloudinit"
+        storage = "local-lvm"
+    }
+
+    disk {
+        slot    = "virtio0"
+        size    = "60G"
+        storage = "local-lvm"
+    }
+
+    pci {
+        id     = 1
+        raw_id = "0000:05:00.1"
+    }
+
+    # VM Cloud-Init Settings
+    os_type = "cloud-init"
+
+    # IP Address and Gateway
+    ipconfig0 = "ip=192.168.1.249/24,gw=192.168.1.1"
+
+    tags = "nas,backup,terraform"
+
+    lifecycle {
+        ignore_changes = [
+            ipconfig0,
+            disk,
+        ]
+    }
+}
